b4dffb609478eb26c1fb4dacb45537681619644e
fenfang2711 <80599976+fenfang2711@users.noreply.github.com>
2021-03-14 20:51:53 -0400

    Create mysqrt.java

---
+++ mysqrt.java
@@ -0,0 +1,13 @@
+public class mysqrt {
+
+    private static int my_sqrt(int x)
+    {
+        return (int) Math.sqrt(x);
+
+    }
+
+    public static void main(String[] args) {
+        int a=39;
+        System.out.println(my_sqrt(a));
+    }
+}
